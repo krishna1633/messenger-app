1ba6700671a88960b6c543816c430859c238dc88
krishna1633 <49479107+krishna1633@users.noreply.github.com>
2024-10-19 16:15:38 +0530

    Update friendController.ts

--- messenger-api/src/controllers/friendController.ts
+++ messenger-api/src/controllers/friendController.ts
@@ -1,9 +1,6 @@
-// src/controllers/friendController.ts
-
 import { Request, Response } from 'express';
 import { friends } from '../models/dataModel';
 
-// Fetch the list of friends
 export function getFriends(req: Request, res: Response) {
   res.json(friends);
 }
